1f1978eca22b810c4c4e572f4226a2c8a73aa392
bunsai <bunsai090saporno@gmail.com>
2026-04-28 05:43:25 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { LandingPage } from './pages/LandingPage';
 import { LoginPage } from './pages/auth/LoginPage';
+import { SignUpPage } from './pages/auth/SignUpPage';
 import { AdminDashboard } from './pages/admin/AdminDashboard';
 import { TeacherDashboard } from './pages/teacher/TeacherDashboard';
 import { ParentPortal } from './pages/parent/ParentPortal';
@@ -11,6 +12,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/teacher" element={<TeacherDashboard />} />
         <Route path="/parent" element={<ParentPortal />} />
