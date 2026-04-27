7bab6dfa329c4d7b2fdf0f761a73beab8c6f3894
bunsai <bunsai090saporno@gmail.com>
2026-04-28 04:57:27 +0800

    Update ClassroomsView.tsx

--- src/components/admin/ClassroomsView.tsx
+++ src/components/admin/ClassroomsView.tsx
@@ -114,7 +114,7 @@ export function ClassroomsView() {
                     <SelectContent>
                       {teachers.map((t) =>
                       <SelectItem key={t.id} value={t.id}>
-                          {t.name}
+                          {t.firstName} {t.lastName}
                         </SelectItem>
                       )}
                     </SelectContent>
@@ -373,7 +373,7 @@ export function ClassroomsView() {
                           {teacher ?
                         <div className="flex items-center text-xs text-muted-foreground">
                               <Users className="w-3 h-3 mr-1.5" />
-                              {teacher.name}
+                              {teacher.firstName} {teacher.lastName}
                             </div> :
 
                         <Badge
